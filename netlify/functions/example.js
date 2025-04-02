a3f611cc0b4d4494b240f73f1a0a60f4d84861d7
Vivekkumar3773 <vivekrocky5223@gmail.com>
2025-04-02 12:05:31 +0530

    This update for Hosting

--- netlify/functions/example.js
+++ netlify/functions/example.js
@@ -4,9 +4,19 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
+// Access environment variables
+const dbUrl = process.env.DB_URL;
+const apiKey = process.env.API_KEY;
+const baseUrl = process.env.BASE_URL;
+
 // Define your routes
 router.get('/example', (req, res) => {
-    res.json({ message: 'Hello from Netlify Function!' });
+    res.json({
+        message: 'Hello from Netlify Function!',
+        dbUrl,
+        apiKey,
+        baseUrl
+    });
 });
 
 app.use('/.netlify/functions/example', router);
